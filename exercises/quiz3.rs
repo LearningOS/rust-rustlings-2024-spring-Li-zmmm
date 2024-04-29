6e3b6ba3e2eca5e32a4c3b8725a62a5e5d5168f8
lzm <rCore@qq.com>
2024-04-30 00:08:55 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,15 +16,18 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-pub struct ReportCard {
-    pub grade: f32,
+use std::fmt::Display;
+// enum Grade{
+//     Numeric(f32),
+//     Alphabetic(String),
+// }
+pub struct ReportCard <T:Display>{
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl <T:Display>ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -39,6 +42,7 @@ mod tests {
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
             grade: 2.1,
+            // grade:Grade::Numeric(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -52,7 +56,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
